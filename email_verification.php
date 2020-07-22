02fc71b7b6141a64415f3bf63d39c8a4ee3e01f1
Abhishek Mundada <42415137+Vault09@users.noreply.github.com>
2020-07-22 23:52:41 +0530

    Add files via upload

---
+++ email_verification.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>User Registration</title>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+</head>
+
+<body>
+ <div class="container">
+  <br />
+  <h3 align="center">User Registration</h3>
+  <br />
+  
+  <?php
+
+  echo $message;
+  
+  ?>
+  
+ </div>
+</body>
+</html>
